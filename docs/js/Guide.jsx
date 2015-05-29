b74391bd27570b885dab5ee7ac91253f4ddac45b
Levi Tan Ong <levi@byimplication.com>
2015-05-29 20:27:58 +0800

    multiline strings!

--- docs/js/Guide.jsx
+++ docs/js/Guide.jsx
@@ -3,63 +3,71 @@ import Highlight from "./Highlight.jsx";
 import {Plotypus, PlotypusRow, Chart, GroupedBarLayer} from "../../src/js/Plotypus.jsx";
 
 export default class Guide extends React.Component {
-	genData(count, values, cats, series) {
-	  /*
-	    count (Number):           How many datapoints you want.
-	    values (Array(Number)):   Must be of length 2. Min and Max.
-	    cats (Array(String)):     Set of all possible categories.
-	    series (Array(String)):   Set of all possible series.
-	  */
-	  return _.times(count, function(n){
-	    return {
-	      value: (values[1] - values[0]) * Math.random(),
-	      category: _.sample(cats),
-	      series: _.sample(series)
-	    }
-	  })
-	}
-	render() {
-		var data = this.genData(12, [0, 10], ["wat", "derp", "honk"], ["eh"]);
-		return (
-			<div className="Guide split-pane">
-				<div className="sidebar">
-					<div>
-						<h4>Derp</h4>
-						<ul>
-							<li><a>Structure</a></li>
-							<li><a>PlotypusRow</a></li>
-						</ul>
-					</div>
-				</div>
-				<div className="main">
-					<section>
-						<h2>Structure</h2>
-						<div>
-							<p>
-								Building out components in Plotypus is easy.
-							</p>
-							<Highlight className="solarized_dark">
-								{ "<div>wat</div>" }
-							</Highlight>
-						</div>
-					</section>
-					<Plotypus>
-					  <PlotypusRow>
-					    <Chart>
-					      <GroupedBarLayer 
-					        groupOffset={ 1.2 } 
-					        barWidth={ 0.1 }
-					        max={ 10 }
-					        min={ 0 }
-					        categoryField="category"
-					        seriesField="series"
-					        valueField="value"
-					        data={ data } />
-					    </Chart>
-					  </PlotypusRow>
-					</Plotypus>
-				</div>
-			</div>
-		)
-	}
+  genData(count, values, cats, series) {
+    /*
+      count (Number):           How many datapoints you want.
+      values (Array(Number)):   Must be of length 2. Min and Max.
+      cats (Array(String)):     Set of all possible categories.
+      series (Array(String)):   Set of all possible series.
+    */
+    return _.times(count, function(n){
+      return {
+        value: (values[1] - values[0]) * Math.random(),
+        category: _.sample(cats),
+        series: _.sample(series)
+      }
+    })
+  }
+  render() {
+    var data = this.genData(12, [0, 10], ["wat", "derp", "honk"], ["eh"]);
+    return (
+      <div className="Guide split-pane">
+        <div className="sidebar">
+          <div>
+            <h4>Derp</h4>
+            <ul>
+              <li><a>Structure</a></li>
+              <li><a>PlotypusRow</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="main">
+          <section>
+            <h2>Structure</h2>
+            <div>
+              <p>
+                Building out components in Plotypus is easy.
+              </p>
+              <Highlight className="solarized_dark">
+                { 
+                  `<Plotypus>
+  <PlotypusRow>
+    wat
+  </PlotypusRow>
+  <PlotypusRow>
+    derp
+  </PlotypusRow>
+</Plotypus>` }
+              </Highlight>
+            </div>
+          </section>
+          <Plotypus>
+            <PlotypusRow>
+              <Chart>
+                <GroupedBarLayer 
+                  groupOffset={ 1.2 } 
+                  barWidth={ 0.1 }
+                  max={ 10 }
+                  min={ 0 }
+                  categoryField="category"
+                  seriesField="series"
+                  valueField="value"
+                  data={ data } />
+              </Chart>
+            </PlotypusRow>
+          </Plotypus>
+        </div>
+      </div>
+    )
+  }
 }
