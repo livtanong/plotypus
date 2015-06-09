9cfe38c063ee1f5beb023f4341bcd64a156a66fa
Levi Tan Ong <levi@byimplication.com>
2015-06-10 02:47:44 +0800

    fixes bug

--- prerenderHtml.js
+++ prerenderHtml.js
@@ -1,9 +1,10 @@
 var React = require('react');
 var Index = require('./docs/js/index');
-var IndexString = require('./docs/js/index');
+// var IndexString = require('./docs/js/index');
 
 var indexHtml = require('./docs/index.html');
-// var IndexString = React.renderToStaticMarkup(<Index />);
+var IndexString = React.renderToStaticMarkup(React.createElement(Index, {isPrerendered: true}));
+// console.log(IndexString, typeof IndexString);
 
 module.exports = indexHtml.replace('Loading', IndexString);
 
